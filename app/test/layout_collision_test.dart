fb49d73a5861bdf7f6d77df6052dede02d9f975e
xinxinxin <xinxinxin1027@gmail.com>
2026-08-11 18:45:03 +0800

    test: isolate each page and viewport layout regression

--- app/test/layout_collision_test.dart
+++ app/test/layout_collision_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:rainbow_cats/main.dart';
 import 'package:rainbow_cats/src/pages.dart';
 import 'package:rainbow_cats/src/store.dart';
 
@@ -13,31 +12,33 @@ void main() {
   ];
 
   for (final Size size in sizes) {
-    testWidgets('全部十二页在 ${size.width}x${size.height} 无布局异常',
+    for (int index = 0; index < pageNames.length; index += 1) {
+      testWidgets(
+        '${pageNames[index]} 在 ${size.width}x${size.height} 无布局异常',
         (WidgetTester tester) async {
-      await tester.binding.setSurfaceSize(size);
-      addTearDown(() => tester.binding.setSurfaceSize(null));
-      final RainbowStore store = RainbowStore(
-        MemoryRainbowStorage(),
-        clock: () => DateTime(2026, 8, 10, 12),
-      )..seedForTest();
-      for (final Widget page in buildVisualPages(store)) {
-        await tester.pumpWidget(
-          RainbowCatsApp(
-            store: store,
-            visualReview: false,
-            key: UniqueKey(),
-          ),
-        );
-        await tester.pumpWidget(
-          MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: page,
-          ),
-        );
-        await tester.pump(const Duration(milliseconds: 250));
-        expect(tester.takeException(), isNull);
-      }
-    });
+          await tester.binding.setSurfaceSize(size);
+          addTearDown(() => tester.binding.setSurfaceSize(null));
+          final RainbowStore store = RainbowStore(
+            MemoryRainbowStorage(),
+            clock: () => DateTime(2026, 8, 10, 12),
+          )..seedForTest();
+          final List<Widget> pages = buildVisualPages(store);
+          await tester.pumpWidget(
+            MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: pages[index],
+            ),
+          );
+          await tester.pump(const Duration(milliseconds: 250));
+          final Object? exception = tester.takeException();
+          expect(
+            exception,
+            isNull,
+            reason:
+                '${pageNames[index]} @ ${size.width}x${size.height} 出现布局/渲染异常',
+          );
+        },
+      );
+    }
   }
 }
