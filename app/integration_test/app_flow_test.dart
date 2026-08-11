a39a4379f4e906dbbbad4cb6cb147e67bf45ac32
xinxinxin <xinxinxin1027@gmail.com>
2026-08-11 18:13:13 +0800

    fix: import Flutter ValueKey in integration flow

--- app/integration_test/app_flow_test.dart
+++ app/integration_test/app_flow_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:rainbow_cats/main.dart';
